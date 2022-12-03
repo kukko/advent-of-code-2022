f35f0cb40cdcc49415e3843847fd6b92b1e6cf9e
gnyar <gergo.nyarai@cae.com>
2022-12-03 11:47:30 +0100

    Complete second task of the second day.

--- 02/index.py
+++ 02/index.py
@@ -1,3 +1,5 @@
 from functools import reduce
 print(reduce(lambda e, f: e + (6 if f[2] == 1 or f[2] == -2 else 3 if f[2] == 0 else 0) + (f[1] + 1), map(lambda d: d + [d[1] - d[0]], list(map(lambda a: list(map(lambda c: c - 88 if c > 67 else c - 65, map(lambda b: ord(b), a.replace('\n', '').split(
     ' ')))), open('input.txt').readlines()))), 0))
+print(reduce(lambda e, f: e + (6 if f[1] == 3 else 3 if f[1] == 2 else 0) + (f[0] if f[1] == 2 else (f[0] - 1 if f[0] - 1 >= 1 else 3) if f[1] == 1 else (f[0] + 1 if f[0] + 1 <= 3 else 1)), list(map(lambda a: list(map(lambda c: c - 87 if c > 67 else c - 64, map(lambda b: ord(b), a.replace('\n', '').split(
+    ' ')))), open('input.txt').readlines())), 0))
