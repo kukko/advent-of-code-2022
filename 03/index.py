3787e2958e9db119fc6c10bd6db7736ac91318dc
gnyar <gergo.nyarai@cae.com>
2022-12-03 15:41:26 +0100

    Complete second task of the third day.

--- 03/index.py
+++ 03/index.py
@@ -12,3 +12,24 @@ with open('input.txt') as f:
                 else:
                     sum = sum + ord(item) - 38
     print(sum)
+    badgeSum = 0
+    for i in range(0, len(lines), 3):
+        found = False
+        for j in lines[i][0] + lines[i][1]:
+            for k in range(1, 3):
+                contains = False
+                for l in lines[i + k][0] + lines[i + k][1]:
+                    if j == l:
+                        contains = True
+                        break
+                if contains == False:
+                    break
+                elif k == 2:
+                    found = True
+                    if ord(j) > 90:
+                        badgeSum = badgeSum + ord(j) - 96
+                    else:
+                        badgeSum = badgeSum + ord(j) - 38
+            if found == True:
+                break
+    print(badgeSum)
