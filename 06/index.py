c094be0527a187571eb4e6f7f648da31e286bbaf
gnyar <gergo.nyarai@cae.com>
2022-12-08 14:14:56 +0100

    Complete second task of the sixth day.

--- 06/index.py
+++ 06/index.py
@@ -1,14 +1,15 @@
-def findStartOfPacketMarker(dataStream):
-  for i in range(3, len(dataStream)):
+def findStartOfPacketMarker(dataStream, lengthOfMarker):
+  for i in range(lengthOfMarker - 1, len(dataStream)):
     chars = []
-    for j in range(i - 3, i + 1):
+    for j in range(i - (lengthOfMarker - 1), i + 1):
       if dataStream[j] not in chars:
         chars.append(dataStream[j])
       else:
         break
-    if len(chars) == 4:
+    if len(chars) == lengthOfMarker:
       return i + 1
 
 with open('input.txt') as f:
   dataStream = f.readline()
-  print(findStartOfPacketMarker(dataStream))
+  print(findStartOfPacketMarker(dataStream, 4))
+  print(findStartOfPacketMarker(dataStream, 14))
